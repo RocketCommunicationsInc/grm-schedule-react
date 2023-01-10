1b9c4bb4decc2e0c7572045f08c32c540c2c1b9a
Brian <brian@briankiernan.com>
2023-01-10 17:25:44 -0500

    start react table

--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -1,6 +1,18 @@
+import { createColumnHelper, useReactTable } from '@tanstack/react-table';
+import { useMemo } from 'react';
 import './ContactsList.scss';
 
+const columnHelper = createColumnHelper();
+
+const columnDefs = [columnHelper.accessor('_id', {})];
+
 const ContactsList = () => {
+  const columns = useMemo(() => columnDefs, []);
+
+  useReactTable({
+    columns,
+  });
+
   return (
     <div>
       <div>List</div>
