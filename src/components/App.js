b40c1695f7cb7915c27a4ea9892eded4a387f292
Brian <brian@briankiernan.com>
2023-01-10 17:24:17 -0500

    set some initial app data

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
+import { groupByToMap, setGroup } from 'utils/grouping';
+import data from 'data/contacts.json';
 import GlobalStatusBar from './GlobalStatusBar/GlobalStatusBar';
 import ContactsHeader from './ContactsHeader/ContactsHeader';
 import ContactsToolBar from './ContactsToolbar/ContactsToolbar';
@@ -11,6 +13,18 @@ const App = () => {
   const [zoom, setZoom] = useState('8');
   const [view, setView] = useState('List');
 
+  const appData = useMemo(() => {
+    const ends = data.map((c) => c.contactEndTimestamp);
+    const starts = data.map((c) => c.contactBeginTimestamp);
+
+    return {
+      contacts: [...data],
+      regions: setGroup(groupByToMap([...data], (e) => e.contactGround)),
+      start: new Date(Math.min(...starts) * 1000),
+      end: new Date(Math.max(...ends) * 1000),
+    };
+  }, []);
+
   return (
     <>
       <GlobalStatusBar />
@@ -18,12 +32,12 @@ const App = () => {
         <div className='App-main__container'>
           <ContactsHeader />
 
-          <ContactsToolBar {...{ view, setView, setZoom, zoom }} />
+          <ContactsToolBar {...{ view, setView, setZoom, zoom, ...appData }} />
 
           {view === 'List' ? (
-            <ContactsList />
+            <ContactsList {...appData} />
           ) : (
-            <ContactsTimeline zoom={zoom} />
+            <ContactsTimeline {...appData} zoom={zoom} />
           )}
         </div>
       </main>
