b63844c047c829a8311206229cfd2a5fcad7041a
Terry Roe <terryroe@gmail.com>
2023-01-16 15:34:08 -0700

    GRMA-45 Revert some previous changes

--- src/components/ContactsHeader/ContactsHeader.js
+++ src/components/ContactsHeader/ContactsHeader.js
@@ -1,11 +1,10 @@
-import { RuxButton, RuxIcon } from '@astrouxds/react';
+import { RuxButton } from '@astrouxds/react';
 import './ContactsHeader.scss';
 
 const ContactsHeader = ({ isOpen, handleAdd }) => (
   <header className='Contacts-header'>
     <h2>Contacts</h2>
     <RuxButton borderless onClick={handleAdd} disabled={isOpen}>
-      <RuxIcon icon='add-circle-outline' size='extra-small' />
       Add Contact
     </RuxButton>
   </header>
