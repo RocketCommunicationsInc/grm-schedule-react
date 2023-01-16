3029f3d79dae4e3172457ac420bbc5838c52144b
Terry Roe <terryroe@gmail.com>
2023-01-16 11:56:08 -0700

    GRMA-45 Add 'add' icon to Add Contact button

--- src/components/ContactsHeader/ContactsHeader.js
+++ src/components/ContactsHeader/ContactsHeader.js
@@ -1,10 +1,11 @@
-import { RuxButton } from '@astrouxds/react';
+import { RuxButton, RuxIcon } from '@astrouxds/react';
 import './ContactsHeader.scss';
 
 const ContactsHeader = ({ isOpen, handleAdd }) => (
   <header className='Contacts-header'>
     <h2>Contacts</h2>
     <RuxButton borderless onClick={handleAdd} disabled={isOpen}>
+      <RuxIcon icon='add-circle-outline' size='extra-small' />
       Add Contact
     </RuxButton>
   </header>
