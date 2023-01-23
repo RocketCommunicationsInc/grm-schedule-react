d90fac6b53d3247685f2e64a57b4a49dd6ece459
Brian <brian@briankiernan.com>
2023-01-23 12:58:56 -0500

    add some utils

--- src/utils/date.js
+++ src/utils/date.js
@@ -6,3 +6,17 @@ export const getDayOfYear = (date) => {
   const day = Math.floor(diff / oneDay);
   return day;
 };
+
+export const setHhMmSs = (time) => {
+  const date = new Date(time);
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+  return `${hour}:${minute}:${second}`;
+};
+
+export const setDurationMins = (start, end) => {
+  const diffMins = Math.abs((start - end) / (1000 * 60)).toFixed(2);
+  const [mins, seconds] = diffMins.split('.');
+  return `${mins}:${seconds}`;
+};
