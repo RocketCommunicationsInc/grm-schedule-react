c2c1dfafcd8c8c5e1e9ee1f4edfd78757e0a231a
Brian <brian@briankiernan.com>
2023-02-03 11:20:31 -0500

    adjust contact header to be in the header slot

--- src/components/ContactsHeader/ContactsHeader.js
+++ src/components/ContactsHeader/ContactsHeader.js
@@ -2,13 +2,14 @@ import { RuxButton } from '@astrouxds/react';
 import './ContactsHeader.scss';
 
 const ContactsHeader = ({ isOpen, handleAction }) => (
-  <header className='Contacts-header'>
+  <header slot='header' className='Contacts-header'>
     <h2>Contacts</h2>
     <RuxButton
       icon='add'
       borderless
       onClick={() => handleAction('add')}
       disabled={isOpen}
+      size='small'
     >
       Add Contact
     </RuxButton>
