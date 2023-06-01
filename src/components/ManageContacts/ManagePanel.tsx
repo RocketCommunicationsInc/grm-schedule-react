88e104b79affc6a7d946be6809dcf5651270cffd
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-06-01 09:11:10 -0600

    updated contact details css to only apply to contact details inputs

--- src/components/ManageContacts/ManagePanel.tsx
+++ src/components/ManageContacts/ManagePanel.tsx
@@ -25,7 +25,6 @@ const ManagePanel = ({ handleAction }: PropTypes) => {
         Filter Contacts
         <RuxIcon icon='arrow-right' size='small' />
       </div>
-      
       <footer slot='footer' className='manage-panel-footer'>
         <RuxButton secondary onClick={handleClose}>
           Close
