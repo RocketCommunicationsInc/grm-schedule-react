24c65ddaa7e7c9523235b9d4c63c336c22246cef
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-06-02 15:05:36 -0600

    seperate table header to fix scroll bar issue

---
+++ src/common/AstroReactTable/AstroReactTableHeader.tsx
@@ -0,0 +1,41 @@
+import { RuxIcon } from '@astrouxds/react';
+import { flexRender } from '@tanstack/react-table';
+import './AstroReactTable.css';
+
+type PropTypes = {
+  table: any;
+  isSortable: boolean;
+};
+
+export const AstroReactTableHeader = ({
+  table,
+  isSortable = false,
+}: PropTypes) => {
+  return (
+    <header className='Astro-react-table__header'>
+      {table.getFlatHeaders().map(({ id, column, getContext }: any) => (
+        <div
+          key={id}
+          className={
+            isSortable && !!column.getIsSorted()
+              ? 'Astro-react-table__col Astro-react-table__sorted'
+              : 'Astro-react-table__col' || isSortable
+              ? 'Astro-react-table__col Astro-react-table__sortable'
+              : 'Astro-react-table__col'
+          }
+          style={column.columnDef.style}
+          onClick={isSortable ? column.getToggleSortingHandler() : undefined}
+        >
+          {flexRender(column.columnDef.header, getContext())}
+
+          {isSortable &&
+            //@ts-expect-error with types
+            {
+              asc: <RuxIcon icon='arrow-drop-up' size='1.5rem' />,
+              desc: <RuxIcon icon='arrow-drop-down' size='1.5rem' />,
+            }[column.getIsSorted()]}
+        </div>
+      ))}
+    </header>
+  );
+};
