c41d06827362c0e3b2892beb16dccfa96e753bff
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-06-13 13:52:27 -0600

    working on search bar, some logic completed

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,19 +1,43 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import { RuxInput } from '@astrouxds/react';
-// import contacts from '../../data/contacts.json';
 import './SearchBar.css';
 
 const SearchBar = () => {
-  // const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState('');
+
+  const tRow = document.getElementsByClassName('Astro-react-table__row');
+  const tCell = document.getElementsByClassName('Astro-react-table__cell');
+  //const rowData = Object.values(tRow).map((row) => row.innerHTML)
+
+  for (let i = 0; i < tRow.length; i++) {
+    const row = tRow[i];
+    console.log(row);
+    let searchVal = false;
+
+    for (let j = 0; j < tCell.length; j++) {
+      const cell = tCell[j];
+      const cellValue = cell.innerHTML.toLowerCase();
+
+      if (cellValue.includes(searchValue)) {
+        searchVal = true;
+      }
+    }
+    //@ts-expect-error
+    row.style.display = searchVal ? '' : 'none';
+  }
+
+  // const searchData = {
+  //   //.filter((item: any) => item.name.toLowerCase.includes(searchValue))
+  // };
 
   return (
-      <RuxInput
-        type='search'
-        placeholder='Search'
-        size='small'
-        className='main-search'
-        // onRuxinput={(event) => setSearchValue(event.target.value)}
-      />
+    <RuxInput
+      type='search'
+      placeholder='Search'
+      size='small'
+      className='main-search'
+      onRuxinput={(event) => setSearchValue(event.target.value)}
+    />
   );
 };
 
