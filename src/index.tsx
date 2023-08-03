d003adabfac18741495ca0bb5240d1f9c6e2edb9
Nic Dominguez <NicDominguez@users.noreply.github.com>
2023-08-02 23:23:17 -0700

    mock-data initial setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { TTCGRMProvider } from '@astrouxds/mock-data';
 import App from 'components/App';
 import AppProvider from 'providers/AppProvider';
 import './index.css';
@@ -8,10 +8,19 @@ import './index.css';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const options = {
+  alertsPercentage: 0 as const,
+  initial: 50,
+  limit: 50,
+};
+
 root.render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <TTCGRMProvider options={options}>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </TTCGRMProvider>
   </React.StrictMode>
 );
